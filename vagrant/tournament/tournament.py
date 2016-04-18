cf068af52bb19879c68f1a75e95d7faa2aa1a58e
Boxuan Zhang <boxuan.zhang@drchrono.com>
2016-04-17 19:35:59 -0700

    Pass test case with basic requirements

--- vagrant/tournament/tournament.py
+++ vagrant/tournament/tournament.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# 
+#
 # tournament.py -- implementation of a Swiss-system tournament
 #
 
@@ -13,25 +13,51 @@ def connect():
 
 def deleteMatches():
     """Remove all the match records from the database."""
-
+    db = connect()
+    c = db.cursor()
+    sql = 'DELETE FROM matches'
+    c.execute(sql)
+    db.commit()
+    db.close()
 
 def deletePlayers():
     """Remove all the player records from the database."""
+    db = connect()
+    c = db.cursor()
+    sql = 'DELETE FROM players'
+    c.execute(sql)
+    db.commit()
+    db.close()
 
 
 def countPlayers():
     """Returns the number of players currently registered."""
+    db = connect()
+    c = db.cursor()
+    query = 'SELECT count(*) FROM players'
+    c.execute(query)
+    rows= c.fetchall()
 
+    count = rows[0][0]
+
+    db.close()
+
+    return count
 
 def registerPlayer(name):
     """Adds a player to the tournament database.
-  
+
     The database assigns a unique serial id number for the player.  (This
     should be handled by your SQL database schema, not in your Python code.)
-  
+
     Args:
       name: the player's full name (need not be unique).
     """
+    db = connect()
+    c = db.cursor()
+    c.execute('INSERT INTO players VALUES (%s)', (name,))
+    db.commit()
+    db.close()
 
 
 def playerStandings():
@@ -47,6 +73,14 @@ def playerStandings():
         wins: the number of matches the player has won
         matches: the number of matches the player has played
     """
+    db = connect()
+    c = db.cursor()
+    query = 'SELECT id, name, wins, games FROM standings'
+    c.execute(query)
+    rows = c.fetchall()
+    db.close()
+
+    return [(row[0], row[1], row[2], row[3]) for row in rows]
 
 
 def reportMatch(winner, loser):
@@ -56,16 +90,21 @@ def reportMatch(winner, loser):
       winner:  the id number of the player who won
       loser:  the id number of the player who lost
     """
- 
- 
+    db = connect()
+    c = db.cursor()
+    c.execute('INSERT INTO matches VALUES (%s, %s)', (winner, loser,))
+    db.commit()
+    db.close()
+
+
 def swissPairings():
     """Returns a list of pairs of players for the next round of a match.
-  
+
     Assuming that there are an even number of players registered, each player
     appears exactly once in the pairings.  Each player is paired with another
     player with an equal or nearly-equal win record, that is, a player adjacent
     to him or her in the standings.
-  
+
     Returns:
       A list of tuples, each of which contains (id1, name1, id2, name2)
         id1: the first player's unique id
@@ -73,5 +112,18 @@ def swissPairings():
         id2: the second player's unique id
         name2: the second player's name
     """
+    standings = playerStandings()
+
+    pairings = []
+    for i in range(0, len(standings) - 1, 2):
+        pairings.append((
+            standings[i][0],
+            standings[i][1],
+            standings[i + 1][0],
+            standings[i + 1][1]
+        ))
+
+    return pairings
+
 
 
--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -6,4 +6,62 @@
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
 
+-- Drop tournament database if it exists
+DROP DATABASE IF EXISTS tournament;
+
+-- Create database
+CREATE DATABASE tournament;
+
+-- Connect to tournament
+\c tournament;
+
+-- Create player table with structure
+--
+-- Column   |   Type    |   Modifiers
+-- ---------+-----------+-----------------
+-- id       |   SERIAL  | primary key
+-- ---------+-----------+-----------------
+-- name     |   text    |   
+-- ---------------------------------------
+CREATE TABLE players (
+    name text,
+    id SERIAL PRIMARY KEY
+);
+
+-- Create matches table with structure
+-- Column   |   Type    |   Modifiers
+-- ---------+-----------+------------------------
+-- id       |   SERIAL  | primary key
+-- ---------+-----------+------------------------
+-- winner   |   integer | reference id in player
+-- ----------------------------------------------
+-- loser    |   integer | reference id in player
+-- ----------------------------------------------
+CREATE TABLE matches (
+    winner integer REFERENCES players,
+    loser integer REFERENCES players,
+    id SERIAL PRIMARY KEY
+);
+
+-- Create a view named stangdings with structure
+--   Column   |  Type   | Modifiers
+--  ----------+---------+-----------
+--  player_id | integer |
+--  name      | text    |
+--  wins      | integer |
+--  played    | integer |
+CREATE VIEW standings AS
+SELECT players.id,
+    players.name,
+    (SELECT count(*) FROM matches WHERE winner = players.id) as wins,
+    (SELECT count(*) FROM matches WHERE winner = players.id or loser = players.id) as games
+FROM players
+GROUP BY players.id
+ORDER BY wins DESC;
+
+
+
+
+
+
 
